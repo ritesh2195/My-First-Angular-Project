9e89c251bfd77e7f0a49bb6f727a15d2186aeed9
Ritesh Ranjan Mishra <riteshranjanmishra938@gmail.com>
2022-09-25 19:01:51 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -37,6 +37,7 @@ const routes:Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
     HttpClientModule
     
--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-user-account',
@@ -8,14 +9,36 @@ import { NgForm } from '@angular/forms';
 })
 export class UserAccountComponent implements OnInit {
 
-  constructor() { }
+  editButton:boolean = true;
+
+  userDetails = []
+
+  constructor(private userService:UsersService) { }
 
   ngOnInit(): void {
+
+    this.userDetails = this.userService.getUserInfo()
   }
+
+  // userForm = new FormGroup({
+  //   name:new FormControl(this.userDetails[0].name),
+  //   email : new FormControl(this.userDetails[0].email)
+  // })
 
   onSubmit(data:NgForm){
 
-    
+    this.editButton = true
+
+  }
+
+  onClickEdit(){
+
+    this.editButton = false;
+  }
+
+  OnCancel(){
+
+    this.editButton = true
   }
 
 }
