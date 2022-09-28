128b117b805ddd735dd51666efb42b3350f065f1
Ritesh Ranjan Mishra <riteshranjanmishra938@gmail.com>
2022-09-28 23:58:28 +0530

    new changes

--- src/app/user-account/user-account.component.ts
+++ src/app/user-account/user-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsersService } from '../services/users.service';
 
 @Component({
@@ -9,35 +9,31 @@ import { UsersService } from '../services/users.service';
 })
 export class UserAccountComponent implements OnInit {
 
-  @ViewChild('userDetails') form:NgForm 
+  userForm:FormGroup;
 
   editButton:boolean = true;
 
+  phone:number = 1234567
+
   userDetails = {}
-
-  name:string = ''
-
-  email:string = ''
-
-  phone:number= 1234567890
 
   constructor(private userService:UsersService) { }
 
   ngOnInit(): void {
 
-    this.name = this.userService.getUserInfo()[0].name
+    this.userForm = new FormGroup({
+      'name':new FormControl(this.userService.getUserInfo()[0].name),
+      'email' : new FormControl(this.userService.getUserInfo()[0].email),
+      'phone':new FormControl(this.phone)
+    })
 
-    this.email = this.userService.getUserInfo()[0].email
   }
 
-  // userForm = new FormGroup({
-  //   name:new FormControl(this.userDetails[0].name),
-  //   email : new FormControl(this.userDetails[0].email)
-  // })
-
-  onSubmit(data:NgForm){
+  onSubmit(){
 
     this.editButton = true
+
+    console.log(this.userForm)
 
   }
 
@@ -49,13 +45,6 @@ export class UserAccountComponent implements OnInit {
   OnCancel(data:NgForm){
 
     this.editButton = true
-
-    data.resetForm({
-      name:this.name,
-      email:this.email,
-      phone:this.phone
-    })
-
   }
 
 }
